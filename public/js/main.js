cf5310f28164be2a12265fd1e05a118b084c5838
JServer <sandersjack141@gmail.com>
2023-04-07 18:09:06 +0100

    Install CSG and added end cone

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+import { CSG } from 'three-csg-ts';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.6, 1200);
@@ -17,9 +18,9 @@ window.addEventListener('resize', () => {
 })
 
 const volumes = [
-    {rout : 150, rin: 139, length: .339, z: -2.15,colour: 0x14D14A},
-    {rout : 279, rin: 139, length: .01 ,z: -1.811,colour: 0xBE61CF},
-    {rout : 279, rin: 267, length: 4.49 ,z: -1.801,colour: 0x16A7F5},
+    {rout : 1.50, rin: 1.39, length: 3.39, z: -21.5, colour: 0x14D14A},
+    {rout : 2.79, rin: 1.39, length: .1 , z: -18.11,colour: 0xBE61CF},
+    {rout : 2.79, rin: 2.67, length: 44.9 ,z: -18.01,colour: 0x16A7F5},
 ];
 
 
@@ -43,60 +44,25 @@ for(let i=0; i<3; i++){
     var material1 = new THREE.MeshBasicMaterial( { color: volumes[i]['colour'] } );
 
     var cylinder1 = new THREE.ExtrudeGeometry(shape, extrudeSettings1 );
-
     var mesh = new THREE.Mesh( cylinder1, material1 );
     mesh.position.set(0,0,volumes[i]['z']);
-    mesh.scale.set(0.01,0.01,1)
     scene.add(mesh)
+
 }
 
-var R =  279;
-var r =  267;
-var cx = 0;
-var cy = 0;
-var sAngle = THREE.MathUtils.degToRad(0);
-var eAngle = THREE.MathUtils.degToRad(360);
+var bigCylinder = new THREE.CylinderGeometry(0.39,2.79, 3.2105, 32 );
+var smallCylinder = new THREE.CylinderGeometry(0.375,2.67, 3.2105, 32 );
 
-let shape = new THREE.Shape();
-shape.absarc(cx, cy, R, sAngle, eAngle);
-shape.absarc(cx, cy, r, eAngle, sAngle, true);
+const material = new THREE.MeshBasicMaterial( {color: 0x90F615} );
+const cylinderBig = new THREE.Mesh( bigCylinder, material );
+const cylinderSmall = new THREE.Mesh( smallCylinder, material );
 
-var extrudeSettings2 = {
-    steps: 20,
-    depth: 0.32105
-};
+const subRes = CSG.subtract(cylinderBig, cylinderSmall);
+subRes.rotation.set(Math.PI / 2,0,0);
+subRes.position.set(0,0,26.89+(3.2105*0.5));
+//var hollowCylinder = subRes.toMesh( material );
 
-var cylinder2 = new THREE.ExtrudeGeometry(shape, extrudeSettings2 );
-const material2 = new THREE.MeshBasicMaterial( { color: 0xBE61CF } );
-var mesh2 = new THREE.Mesh( cylinder2, material2 );
-mesh2.position.set(0,0,2.689);
-mesh2.scale.set(0.01,0.01,1)
-scene.add(mesh2)
-
-var R =  3.9;
-var r =  3.75;
-var cx = 0;
-var cy = 0;
-var sAngle = THREE.MathUtils.degToRad(0);
-var eAngle = THREE.MathUtils.degToRad(360);
-const material3 = new THREE.MeshBasicMaterial( { color: 0x90F615 } );
-let shape1 = new THREE.Shape();
-shape1.absarc(cx, cy, R, sAngle, eAngle);
-shape1.absarc(cx, cy, r, eAngle, sAngle, true);
-
-var extrudeSettings3 = {
-    steps: 2,
-    depth: 0
-};
-
-var cylinder3 = new THREE.ExtrudeGeometry(shape1, extrudeSettings3 );
-
-var mesh3 = new THREE.Mesh( cylinder3, material3 );
-mesh3.position.set(0,0,3.01005);
-mesh3.scale.set(0.01,0.01,1)
-scene.add(mesh3)
-
-
+scene.add( subRes );
 
 // Lights
 const lights = [];
