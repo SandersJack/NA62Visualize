713222498a8d6ad45282551a68c083469e004dd1
JServer <sandersjack141@gmail.com>
2023-04-07 13:03:02 +0100

    Implemeted ThreeJs

--- public/js/main.js
+++ public/js/main.js
@@ -21,14 +21,46 @@ const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
 const boxMaterial = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
 const boxMesh = new THREE.Mesh(boxGeometry, 
 boxMaterial);
-boxMesh.rotation.set(40, 0, 40);
+boxMesh.rotation.set(0, 0, 0);
 scene.add(boxMesh);
+
+// Lights
+const lights = [];
+const lightValues = [
+    {colour: 0x14D14A, intensity: 8, dist: 12, x: 1, y: 0, z: 8},
+    {colour: 0xBE61CF, intensity: 6, dist: 12, x: -2, y: 1, z: -10},
+    {colour: 0x00FFFF, intensity: 3, dist: 10, x: 0, y: 10, z: 1},
+    {colour: 0x00FF00, intensity: 6, dist: 12, x: 0, y: -10, z: -1},
+    {colour: 0x16A7F5, intensity: 6, dist: 12, x: 10, y: 3, z: 0},
+    {colour: 0x90F615, intensity: 6, dist: 12, x: -10, y: -1, z: 0}
+];
+for (let i=0; i<6; i++) {
+    lights[i] = new THREE.PointLight(
+        lightValues[i]['colour'], 
+        lightValues[i]['intensity'], 
+        lightValues[i]['dist']);
+    lights[i].position.set(
+        lightValues[i]['x'], 
+        lightValues[i]['y'], 
+        lightValues[i]['z']);
+    scene.add(lights[i]);
+}
+
+//Trackball Controls for Camera 
+const controls = new TrackballControls(camera, renderer.domElement); 
+controls.rotateSpeed = 4;
+controls.dynamicDampingFactor = 0.15;
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper); // X == red, Y == green, Z == blue
 
 const rendering = function() {
     requestAnimationFrame(rendering);    // Constantly rotate box
-    scene.rotation.z -= 0.005;
-    scene.rotation.x -= 0.01;    
+    //scene.rotation.z -= 0.005;
+    //scene.rotation.x -= 0.01;    
     renderer.render(scene, camera);
+    controls.update();
 };
 
 rendering();
